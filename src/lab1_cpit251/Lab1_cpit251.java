a4cf0acfe51c080a056d3eab0a5ce95318813f18
AbeerSaleh-0 <aboor.600@gmail.com>
2022-09-10 18:31:19 +0300

    My commit.

--- src/lab1_cpit251/Lab1_cpit251.java
+++ src/lab1_cpit251/Lab1_cpit251.java
@@ -10,9 +10,8 @@ public class Lab1_cpit251 {
         
         System.out.println("Hello, I'm Sumaih");
         
-        System.out.println("Hello, I'm Abeer"); 
-        
-        
+        System.out.println("Hello, I'm Abeer");
+
     }
     
 }
