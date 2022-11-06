0b7bbbdc3ba5050335e6bdf30e4ad81513ee920a
Waad_Alamri <weeedo01421@hotmail.com>
2022-11-06 23:16:37 +0300

    Third commit by Waad, edit mian gui

--- src/NewJFrame.java
+++ src/NewJFrame.java
@@ -1,23 +1,10 @@
 
-import java.awt.Image;
-import java.awt.image.ImageProducer;
-import javax.swing.ImageIcon;
-
-
 public class NewJFrame extends javax.swing.JFrame {
 
     public Customer customer = new Customer();
     
-    // background
-    ImageIcon ba = new ImageIcon("background.png");
-    Image b1 = ba.getImage();
-    Image reb1 = b1.getScaledInstance(580, 552, java.awt.Image.SCALE_SMOOTH);
-    ImageIcon imageB11 = new ImageIcon(reb1);
     public NewJFrame() {
         initComponents();
-        this.setLocationRelativeTo(null);
-        jLabel1.setText("");
-        jLabel1.setIcon(imageB11);
     }
 
     /**
@@ -33,11 +20,10 @@ public class NewJFrame extends javax.swing.JFrame {
         search = new javax.swing.JButton();
         Buy = new javax.swing.JButton();
         Return = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        Add_book = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBackground(new java.awt.Color(255, 255, 255));
 
         Borrow.setBackground(new java.awt.Color(153, 153, 255));
         Borrow.setFont(new java.awt.Font("Bodoni MT", 0, 18)); // NOI18N
@@ -79,13 +65,16 @@ public class NewJFrame extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setBackground(new java.awt.Color(153, 153, 255));
-        jButton1.setFont(new java.awt.Font("Bodoni MT", 0, 18)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(0, 0, 102));
-        jButton1.setText("Add Book");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/background.png"))); // NOI18N
+
+        Add_book.setBackground(new java.awt.Color(153, 153, 255));
+        Add_book.setFont(new java.awt.Font("Bodoni MT", 0, 18)); // NOI18N
+        Add_book.setForeground(new java.awt.Color(0, 0, 102));
+        Add_book.setText("Add book");
+        Add_book.setActionCommand("Add book");
+        Add_book.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                Add_bookActionPerformed(evt);
             }
         });
 
@@ -96,43 +85,40 @@ public class NewJFrame extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(55, 55, 55)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Add_book, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(Return, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Buy, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(Borrow, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(search, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 509, Short.MAX_VALUE))
+                    .addComponent(search, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Buy, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(932, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(45, 45, 45)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(62, 62, 62)
                 .addComponent(search, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(43, 43, 43)
                 .addComponent(Borrow, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(44, 44, 44)
                 .addComponent(Buy, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(48, 48, 48)
+                .addGap(43, 43, 43)
                 .addComponent(Return, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(45, 45, 45)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(77, Short.MAX_VALUE))
-            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(Add_book, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(186, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void BorrowActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BorrowActionPerformed
 
-    private void ReturnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ReturnActionPerformed
-        
-        Return r = new Return();
-        r.setVisible(true);
-    }//GEN-LAST:event_ReturnActionPerformed
+        customer.borrow();
+    }//GEN-LAST:event_BorrowActionPerformed
 
     private void BuyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BuyActionPerformed
 
@@ -140,14 +126,20 @@ public class NewJFrame extends javax.swing.JFrame {
         s.setVisible(true);
     }//GEN-LAST:event_BuyActionPerformed
 
+    private void ReturnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ReturnActionPerformed
+
+        Return r = new Return();
+        r.setVisible(true);
+    }//GEN-LAST:event_ReturnActionPerformed
+
     private void searchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchActionPerformed
 
         customer.search();
     }//GEN-LAST:event_searchActionPerformed
 
-    private void BorrowActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BorrowActionPerformed
-        customer.borrow();
-    }//GEN-LAST:event_BorrowActionPerformed
+    private void Add_bookActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Add_bookActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_Add_bookActionPerformed
 
     /**
      * @param args the command line arguments
@@ -185,10 +177,10 @@ public class NewJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Add_book;
     private javax.swing.JButton Borrow;
     private javax.swing.JButton Buy;
     private javax.swing.JButton Return;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JButton search;
     // End of variables declaration//GEN-END:variables
